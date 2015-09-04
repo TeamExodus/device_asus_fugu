dab890c606c67050c4c309ef409820db273eb020
Dave Kessler <activethrasher00@gmail.com>
2015-09-04 00:37:50 -0400

    product spect for exodus_fugu
    Change-Id: Ic0dffa4f8986f03490c6d4b6b5251db19271bf87

--- exodus_fugu.mk
+++ exodus_fugu.mk
@@ -8,16 +8,15 @@ $(call inherit-product, vendor/exodus/config/common_full_tv.mk)
 # Inherit device configuration
 $(call inherit-product, device/asus/fugu/aosp_fugu.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/asus/fugu/overlay-cm
+DEVICE_PACKAGE_OVERLAYS += device/asus/fugu/overlay
 
 ## Device identifier. This must come after all inclusions
+PRODUCT_NAME := exodus_fugu
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus Player
+PRODUCT_MANUFACTURER := Asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=fugu \
     BUILD_FINGERPRINT=google/fugu/fugu:5.1.1/LMY47V/1836172:user/release-keys \
     PRIVATE_BUILD_DESC="fugu-user 5.1.1 LMY47V 1836172 release-keys"
-
-PRODUCT_NAME := exodus_fugu
-PRODUCT_BRAND := google
-PRODUCT_MODEL := Nexus Player
-PRODUCT_MANUFACTURER := Asus
